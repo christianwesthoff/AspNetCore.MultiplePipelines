06571c75624fb43943f0ee0f2004713cea4ea9c6
Christian Westhoff <westhoffchristian1@gmail.com>
2019-10-29 02:51:52 +0100

    WIP

---
+++ mass-transit/Startup.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WebApiContrib.Core;
+
+namespace mass_transit
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseBranchWithServices("/api", 
+                services => { services.AddControllers(); }, api =>
+            {
+                if (env.IsDevelopment())
+                {
+                    api.UseDeveloperExceptionPage();
+                }
+
+                api.UseHttpsRedirection();
+
+                api.UseRouting();
+
+                api.UseAuthorization();
+
+                api.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+            });
+            app.UseBranchWithServices("/api2", 
+                services => { services.AddControllers(); }, api =>
+                {
+                    if (env.IsDevelopment())
+                    {
+                        api.UseDeveloperExceptionPage();
+                    }
+
+                    api.UseHttpsRedirection();
+
+                    api.UseRouting();
+
+                    api.UseAuthorization();
+
+                    api.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+                });
+        }
+    }
+}
---
+++ mass-transit/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace mass_transit
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
